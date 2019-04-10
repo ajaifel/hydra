782ff1c3f8663e16ffa8c73aac098b9d8aad0637
Jack Baxter <jackbaxter007@btinternet.com>
2019-04-11 00:55:52 +0100

    Initial commit

---
+++ app/src/main/java/com/btinternet/jackbaxter007/hydra/MainActivity.java
@@ -0,0 +1,48 @@
+package com.btinternet.jackbaxter007.hydra;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.text);
+// ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.guildwars2.com/v2/account?access_token=D0DBBF8D-B1FE-564B-BE18-2557045FEC6EE0E463D5-F286-4E86-AE84-F8DDAFF454A2";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+
+}

1c409e91b06afd8848c83b86647b6f2809e2aca1
Jack Baxter <jackbaxter007@btinternet.com>
2019-04-16 11:28:15 +0100

    Gson tested and GW2Account created

--- app/src/main/java/com/btinternet/jackbaxter007/hydra/MainActivity.java
+++ app/src/main/java/com/btinternet/jackbaxter007/hydra/MainActivity.java
@@ -8,8 +8,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.*;
+import com.btinternet.jackbaxter007.hydra.Model.GW2Data.Account;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,25 +27,41 @@ public class MainActivity extends AppCompatActivity {
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://api.guildwars2.com/v2/account?access_token=D0DBBF8D-B1FE-564B-BE18-2557045FEC6EE0E463D5-F286-4E86-AE84-F8DDAFF454A2";
+        String url ="https://api.guildwars2.com/v2/tokeninfo?access_token=D0DBBF8D-B1FE-564B-BE18-2557045FEC6EE0E463D5-F286-4E86-AE84-F8DDAFF454A2";
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        textView.setText("Response is: "+ response);
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                textView.setText("That didn't work!");
+//            }
+//        });
+        final Gson gson = new GsonBuilder().create();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response);
+                    public void onResponse(JSONObject response) {
+                        Account account = gson.fromJson(response.toString(), Account.class);
+                        textView.setText("Response: " + account.getID());
                     }
                 }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
 
 // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
 
     }
 
---
+++ app/src/main/java/com/btinternet/jackbaxter007/hydra/Model/GW2Data/Account.java
@@ -0,0 +1,14 @@
+package com.btinternet.jackbaxter007.hydra.Model.GW2Data;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class Account {
+    private String id;
+    private String name;
+    private List permissions;
+
+    public String getID() {
+        return id;
+    }
+}
